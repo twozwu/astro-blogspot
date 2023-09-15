1ab08a0555a6f18148a028bd8ae24073d88574d6
at3254 康進 <honter_tribor@hotmail.com>
2023-09-15 17:41:24 +0800

    test

---
+++ src/services/global.ts
@@ -0,0 +1,3 @@
+let storage = {  };
+
+export { storage };
---
+++ src/services/util.vue
@@ -0,0 +1,27 @@
+<template>{{ storage }}asdfasdfsad
+{{  props.storage }}
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { storage } from "@/services/global.ts";
+
+const props = defineProps(['storage']);
+let theme = ref({});
+const translation  = ref();
+onMounted(() => {
+    theme.value = { ...localStorage };
+    // storage = { ...localStorage };
+    // theme.value = window.localeStorage.getItem("theme");
+    // take the correct translation from all translations
+    // translation.value = JSON.parse(
+    //     translations[
+    //         Object.keys(translations).find((key) => key.includes(userLocale))
+    //     ]
+    // );
+});
+defineExpose({ theme });
+</script>
+
+<style>
+</style>
