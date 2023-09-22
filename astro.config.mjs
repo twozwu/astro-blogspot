d53e09033d917fed0a9d0a105f61f9bef6e09ddc
Mix <honter_tribor@hotmail.com>
2023-09-23 02:46:39 +0800

    update install tailwind in different way

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+// Custom Packages
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), vue()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), vue(), tailwind()]
 });
